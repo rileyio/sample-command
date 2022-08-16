2c831f29ca3e248dbe7d73e74e6e7ee7bda3f4f8
Emma <6752931+rileyio@users.noreply.github.com>
2022-08-15 21:20:15 -0300

    Update plugin.ts

--- plugin.ts
+++ plugin.ts
@@ -5,39 +5,49 @@
  * @version 1.0.1
  */
 
-import { Plugin } from '../../src/index'
-import { RoutedInteraction } from '../../src/router'
-import { SlashCommandBuilder } from 'discord.js'
-
-export class SampleCommandPlugin extends Plugin {
-  config = { testProp: false }
-
-  constructor() {
-    super()
-    console.log('SampleCommand Plugin Loaded')
-  }
-
-  public onEnabled() {
-    this.bot.Router.addRoute({
-      category: 'Plugin',
-      controller: this.routeCommand,
-      name: 'test',
-      permissions: {
-        defaultEnabled: false,
-        serverOnly: false
-      },
-      plugin: this,
-      slash: new SlashCommandBuilder().setName('test').setDescription('Testing Plugin'),
-      type: 'interaction'
-    })
-  }
-
-  public async routeCommand(plugin: SampleCommandPlugin, routed: RoutedInteraction) {
-    console.log('routeCommand')
-    return await routed.reply('Should Reply', true)
-  }
-}
-
-export default function () {
-  return new SampleCommandPlugin()
-}
+ import { Plugin } from '../../src/index';
+ import { RoutedInteraction } from '../../src/router';
+ import { SlashCommandBuilder } from 'discord.js';
+ 
+ export class SampleCommandPlugin extends Plugin {
+   config = { testProp: false };
+ 
+   constructor() {
+     super();
+     console.log('SampleCommand Plugin Loaded');
+   }
+ 
+   public async onEnabled() {
+     await this.bot.Router.addRoute({
+       category: 'Plugin',
+       controller: this.routeCommand,
+       name: 'test',
+       permissions: {
+         defaultEnabled: false,
+         serverOnly: false,
+       },
+       plugin: this,
+       slash: new SlashCommandBuilder()
+         .setName('test')
+         .setDescription('Testing Plugin'),
+       type: 'interaction',
+     });
+   }
+ 
+   public async onDisabled() {
+     await this.bot.Router.removeRoute('test');
+   }
+ 
+   public async routeCommand(
+     plugin: SampleCommandPlugin,
+     routed: RoutedInteraction
+   ) {
+     console.log('routeCommand');
+     return await routed.reply('Should Reply', true);
+   }
+ }
+ 
+ export default function () {
+   return new SampleCommandPlugin();
+ }
+ 
